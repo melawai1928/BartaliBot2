45f46b18e6c6c48e36463921e2489566ad1ad9c4
IMODIL <mdil181296@gmail.com>
2022-04-19 23:10:37 +0700

    command
    thumbnail per class

--- command/fn.js
+++ command/fn.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const cheerio = require("cheerio");
 const rp = require("request-promise");
 const { head } = require("request");
+const { get } = require("request");
 
 module.exports.run = async (bot, message, args, msg) =>{
     let profile = args.slice(0).join('+');
@@ -22,13 +23,82 @@ module.exports.run = async (bot, message, args, msg) =>{
         const guild = $("#wrap > div > div.container > article > div > div > div.box_list_area > ul > li > div.state > a").text();
         const link = $("#wrap > div > div.container > article > div > div > div.box_list_area > ul > li > div.title > a").attr("href");
 
-        const embed = new Discord.RichEmbed()
+   
+        //embed
+        const finfo = new Discord.RichEmbed()
         .setDescription(`**Family Name:** [**${fname}**](${link})\n**Character Name:** ${cname}\n**Class:** ${classs}\n**Guild:** ${guild}`)
-        .setThumbnail('https://www.onlygfx.com/wp-content/uploads/2017/11/wanted-stamp-3.png')
-        .setColor("#0051FF")
-        .setTimestamp()
         .setFooter(`Hunted by: ${message.author.tag}`)
-        message.channel.send(embed)
+        //embed tumbnail
+
+        if ( `${classs}` === `Archer`) {
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character29.jpg')
+        }
+        else if(`${classs}` === `Berserker`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character12.jpg')
+        }
+        else if(`${classs}` === `Corsair`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character10.jpg')
+        }
+        else if(`${classs}` === `Dark Knight`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character27.jpg')
+        }
+        else if(`${classs}` === `Drakania`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character7.jpg')
+        }
+        else if(`${classs}` === `Guardian`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character5.jpg')
+        }
+        else if(`${classs}` === `Hashashin`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character1.jpg')
+        }
+        else if(`${classs}` === `Kunoichi`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character25.jpg')
+        }
+        else if(`${classs}` === `Lahn`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character11.jpg')
+        }
+        else if(`${classs}` === `Maehwa`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character21.jpg')
+        }
+        else if(`${classs}` === `Mystic`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character26.jpg')
+        }
+        else if(`${classs}` === `Musa`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character20.jpg')
+        }
+        else if(`${classs}` === `Ninja`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character26.jpg')
+        }
+        else if(`${classs}` === `Nova`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character9.jpg')
+        }
+        else if(`${classs}` === `Ranger`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character4.jpg')
+        }
+        else if(`${classs}` === `Shai`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character17.jpg')
+        }
+        else if(`${classs}` === `Sorceress`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character8.jpg')
+        }
+        else if(`${classs}` === `Striker`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character19.jpg')
+        }
+        else if(`${classs}` === `Tamer`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character16.jpg')
+        }
+        else if(`${classs}` === `Witch`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character31.jpg')
+        }
+        else if(`${classs}` === `Wizard`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character28.jpg')
+        }
+        else if(`${classs}` === `Valkyrie`){
+            finfo.setThumbnail('https://s1.pearlcdn.com/SEA/contents/img/common/character/character24.jpg')
+        }
+
+        message.channel.send(finfo)
+        
     })
 }
 
